d09a6db9573b7a3742102a4f5c8f2342d4f35662
mmurze <70692966+mmurze@users.noreply.github.com>
2024-03-01 17:47:43 +0300

    lab_4

---
+++ lab_4/framework.h
@@ -0,0 +1,26 @@
+﻿// header.h: включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта
+//
+
+#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
+// Файлы заголовков Windows
+#include <windows.h>
+#include <Windows.h>
+#include <windowsx.h>
+// Файлы заголовков среды выполнения C
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <assert.h>
+#include <thread>
+#include<iostream>
+
+#include <corecrt_math_defines.h>
+#include <dxgi.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
